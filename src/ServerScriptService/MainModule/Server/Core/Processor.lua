bc9ccb8f201413b837f79a20933b34489634ab9b
crywink <30543384+crywink@users.noreply.github.com>
2020-08-25 12:04:36 -0500

    Fixed whitespace bug

--- src/ServerScriptService/MainModule/Server/Core/Processor.lua
+++ src/ServerScriptService/MainModule/Server/Core/Processor.lua
@@ -25,10 +25,10 @@ return function()
 	end
 	
 	Server.ResolveToPlayers = function(Player, String, Wrap, DisableShortcuts)
-		local StringLower = lower(String)
+		local StringLower = lower(String):gsub("%s","")
 		local Wrapper = Service.PlayerWrapper(Player)
 		local Return
-		
+			
 		if StringLower == "me" then
 			Return = Player
 		elseif StringLower == "others" then
